ad0b885a275dc2f0d6dc6f3241bff7968d642557
mclarenmonocacy <w1313746@myfcc.frederick.edu>
2025-10-29 15:47:22 -0400

    Updated Doc

--- src/TransitMap.java
+++ src/TransitMap.java
@@ -1,2 +1,23 @@
+import java.util.List;
+
 public class TransitMap {
+
+
+
+
+
+
+    public List<String> calcShortestPath(List<String> currentPath, TransitNode node){
+        List<String> shortestPath = null;
+        float shortestPathDistance = Float.MAX_VALUE;
+        for (TransitNode newNode : node.getConnections()){
+            float pathDistance = calcShortestPath(currentPath + self, newNode);
+            List<String> newPath = calcShortestPath(currentPath + self, newNode);
+            if (pathDistance < shortestPathDistance) shortestPath = newPath;
+        }
+        if (shortestPath == null) return null;
+        return shortestPath;
+    }
+
+
 }
