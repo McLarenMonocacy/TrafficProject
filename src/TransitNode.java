3a68aec896902f0c3bbf85d2aa239eb3bf7e8c69
Steve-Random <snanafokou070@myfcc.frederick.edu>
2025-11-01 04:19:05 -0400

    Implemented TransitNode, needs careful revision, unit test not made yet.

--- src/TransitNode.java
+++ src/TransitNode.java
@@ -1,5 +1,47 @@
-public class TransitNode {
-    private class TransitConnection{
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
+public class TransitNode {
+    public TransitNode (String id){
+        id = this.id;
+    }
+
+    String id;
+    List< TransitConnection > connectionList;
+    Map< String, List< String > > travelTable;
+
+    private static class TransitConnection{
+        public TransitConnection( String id, float distance, float time ){
+            id = this.idOfConnectedNode;
+            distance = this.distance;
+            time = this.time;
+        }
+
+        Queue<Commuter>transitConnection;
+        String idOfConnectedNode;
+        float distance;
+        int time;
+    }
+
+    public boolean addConnection(TransitNode node, float distance, float time){
+        connectionList.add( new TransitNode.TransitConnection( node.id, distance, time));
+        return true;
+    }
+
+    public void receiveCommuters( TransitVehicle vehicle ){
+        /* while vehicle is not empty
+        remove a commuter (Idk in what data structure the commuters in the vehicle will be stored )
+        check his transitConnection
+        place commuter in the appropriate transitConnect queue
+         */
+    }
+
+    public String getTransitNodeId(){
+        return id;
+    }
+    public List< TransitConnection > getConnectedNodes(){
+        return connectionList;
     }
 }
+
