bfed01918bc7b185956d870dabdbfd71990ca2b5
mclarenmonocacy <w1313746@myfcc.frederick.edu>
2025-11-12 12:45:57 -0500

    Start of SimulationEngine (WIP)

--- src/SimulationEngine.java
+++ src/SimulationEngine.java
@@ -1,2 +1,37 @@
 public class SimulationEngine {
+    private float currentTime;
+    private float runTime;
+    private TransitMap transitMap;
+    private ArrivalProcess arrivals;
+
+    public SimulationEngine(String nodeData, float runTime){
+        currentTime = 0;
+        this.runTime = runTime;
+        transitMap = TransitMap.loadNodes(nodeData);
+        transitMap.genPathTables();
+        arrivals = new ArrivalProcess(1,transitMap);
+    }
+
+    public String run(){
+        while (currentTime < runTime){
+            //TODO: should we use floats or doubles for time purposes?
+            float nextArrival = (float) arrivals.getNextArrivalTime();
+            //checkNodeEvents(); somehow determine if the next event should be a node event
+        }
+
+
+
+
+        return "Output data goes here";
+    }
+
+    private boolean checkNodeEvents(){
+        for (TransitNode node : transitMap.getNodes()){
+            for (TransitConnection connection : node.getConnections()){
+                //Check if a departure can be made
+            }
+        }
+        //somehow return the event that needs done
+        return false;
+    }
 }
